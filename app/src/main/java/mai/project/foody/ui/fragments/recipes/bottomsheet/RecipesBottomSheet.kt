b1f1ddd0939c42d1e2d1cec510b97a1d256bd3d4
DDPlay123 <a70325000@gmail.com>
2023-06-25 21:52:51 +0800

    feat: build RecipesBottomSheet

---
+++ app/src/main/java/mai/project/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -0,0 +1,8 @@
+package mai.project.foody.ui.fragments.recipes.bottomsheet
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+
+}
