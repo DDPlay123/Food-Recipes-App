57a412188f3623e31d299fbf31117b022e29ca5c
DDPlay123 <a70325000@gmail.com>
2023-06-26 01:23:07 +0800

    feat: build RecipesBottomSheet done

--- app/src/main/java/mai/project/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
+++ app/src/main/java/mai/project/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -1,18 +1,95 @@
 package mai.project.foody.ui.fragments.recipes.bottomsheet
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import kotlinx.coroutines.launch
 import mai.project.foody.R
 import mai.project.foody.databinding.RecipesBottomSheetBinding
 import mai.project.foody.ui.fragments.BaseBottomSheetDialogFragment
+import mai.project.foody.util.Constants
+import mai.project.foody.util.Method
+import mai.project.foody.viewmodels.RecipesViewModel
+import java.util.Locale
 
 class RecipesBottomSheet : BaseBottomSheetDialogFragment<RecipesBottomSheetBinding>(R.layout.recipes_bottom_sheet) {
+
+    private lateinit var recipesViewModel: RecipesViewModel
+
+    private var mealTypeChip = Constants.DEFAULT_MEAL_TYPE
+    private var mealTypeChipId = 0
+    private var dietTypeChip = Constants.DEFAULT_DIET_TYPE
+    private var dietTypeChipId = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipesViewModel = ViewModelProvider(requireActivity())[RecipesViewModel::class.java]
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        doObserve()
+        setListener()
+    }
+
+    private fun doObserve() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                launch {
+                    recipesViewModel.readMealAndDietType.asLiveData().observe(viewLifecycleOwner) { value ->
+                        mealTypeChip = value.selectedMealType
+                        dietTypeChip = value.selectedDietType
+
+                        updateChip(value.selectedMealTypeId, binding.chipGroupMealType)
+                        updateChip(value.selectedDietTypeId, binding.chipGroupDietType)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setListener() {
+        binding.apply {
+            chipGroupMealType.setOnCheckedStateChangeListener { group, _ ->
+                val chip = group.findViewById<Chip>(group.checkedChipId)
+                val selectedMealType = chip.text.toString().lowercase(Locale.ROOT)
+                mealTypeChip = selectedMealType
+                mealTypeChipId = group.checkedChipId
+            }
+
+            chipGroupDietType.setOnCheckedStateChangeListener { group, _ ->
+                val chip = group.findViewById<Chip>(group.checkedChipId)
+                val selectedMealType = chip.text.toString().lowercase(Locale.ROOT)
+                dietTypeChip = selectedMealType
+                dietTypeChipId = group.checkedChipId
+            }
+
+            btnApply.setOnClickListener {
+                recipesViewModel.saveMealAndDietType(
+                    mealTypeChip,
+                    mealTypeChipId,
+                    dietTypeChip,
+                    dietTypeChipId
+                ).run { dismiss() }
+            }
+        }
+    }
+
+    private fun updateChip(chipId: Int, chipGroup: ChipGroup) {
+        if (chipId != 0) {
+            try {
+                chipGroup.findViewById<Chip>(chipId).isChecked = true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Method.logE("RecipesBottomSheet", "updateChip", e)
+            }
+        }
     }
 }
--- app/src/main/java/mai/project/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/mai/project/foody/viewmodels/RecipesViewModel.kt
@@ -2,21 +2,47 @@ package mai.project.foody.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import mai.project.foody.data.DataStoreRepository
 import mai.project.foody.util.Constants
 import javax.inject.Inject
 
+@HiltViewModel
 class RecipesViewModel @Inject constructor(
+    private val dataStoreRepository: DataStoreRepository,
     application: Application
 ): AndroidViewModel(application) {
 
+    private var mealType = Constants.DEFAULT_MEAL_TYPE
+    private var dietType = Constants.DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
+
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
+
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
         queries[Constants.QUERY_NUMBER] = Constants.DEFAULT_RECIPES_NUMBER
         queries[Constants.QUERY_API_KEY] = Constants.API_KEY
-        queries[Constants.QUERY_TYPE] = Constants.DEFAULT_MEAL_TYPE
-        queries[Constants.QUERY_DIET] = Constants.DEFAULT_DIET_TYPE
+        queries[Constants.QUERY_TYPE] = mealType
+        queries[Constants.QUERY_DIET] = dietType
         queries[Constants.QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[Constants.QUERY_FILL_INGREDIENTS] = "true"
+
         return queries
     }
 }
