511b5bab06e4701f000cd55fe5a3b5eb0daa79c8
DDPlay123 <a70325000@gmail.com>
2023-06-29 00:55:53 +0800

    feat: add naviagation method 2

--- app/src/main/java/mai/project/foody/ui/MainActivity.kt
+++ app/src/main/java/mai/project/foody/ui/MainActivity.kt
@@ -3,8 +3,13 @@ package mai.project.foody.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import mai.project.foody.R
@@ -13,7 +18,8 @@ import mai.project.foody.databinding.ActivityMainBinding
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var bottomNavigationView: BottomNavigationView
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +29,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun doInitialize() {
-        bottomNavigationView = binding.bottomNavigation
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigationHost) as NavHostFragment
-        val navController = navHostFragment.navController
+        // Method #1
+//        val bottomNavigationView = binding.bottomNavigation
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigationHost) as NavHostFragment
+//        val navController = navHostFragment.navController
+//
+//        NavigationUI.setupWithNavController(bottomNavigationView, navController)
 
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        // Method #2
+        navController = findNavController(R.id.navigationHost)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        binding.bottomNavigation.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    // Method #2
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
