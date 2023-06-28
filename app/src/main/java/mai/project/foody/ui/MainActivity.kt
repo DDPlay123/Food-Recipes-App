1924d6c5c727835d5b00ee570909b5b313aefa68
DDPlay123 <a70325000@gmail.com>
2023-06-29 02:13:05 +0800

    feat: add icon

--- app/src/main/java/mai/project/foody/ui/MainActivity.kt
+++ app/src/main/java/mai/project/foody/ui/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FoodRecipesApp)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         doInitialize()
